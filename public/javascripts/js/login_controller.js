b7017b1b127dfd8cb904bbd1ac976b0fcbf0ea46
Luis Conde <lconder@MacBook-Pro-de-Luis.local>
2017-06-13 08:38:19 -0500

    FIX

---
+++ public/javascripts/js/login_controller.js
@@ -0,0 +1,58 @@
+function submitForm(){
+
+
+	var user = $("#user").val();
+	var password = $("#password").val();
+	console.log($.trim(user))
+	console.log($.trim(password))
+
+	if($.trim(user).length==0 || $.trim(password).length==0){
+		console.log("por favor,  llena todos los campos");
+		$('#alert').html("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert'>&times;</button>Por favor llena todos los campos</div>");
+	}else{
+		$('#alert').empty();
+		$.ajax({
+			type: 'POST',
+			url: '/login_web',
+			data: JSON.stringify({user: user, password: password}),
+			success: function(data) {
+
+				if(data.error == 1){
+					if(data.desc == "no se ha encontrado ningún usuario"){
+						$('#alert').html("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert'>&times;</button>No existe el usuario o el password es incorrecto, verifica.</div>");
+					}else if(data.desc == "error servidor"){
+						$('#alert').html("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert'>&times;</button>Ha ocurrido un error intentalo de nuevo, por favor.</div>");
+					}
+				}
+				else if(data.error == 0)
+				{
+					if(data.active==0){
+						swal("Convenio no activado", "El usuario y password con el que se intenta acceder se encuentra actualmente desactivado.")
+					}else{
+						$('#alert').html("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert'>&times;</button>Inicio de sesión exitoso.</div>");
+						if(data.level==0){
+							window.location = '/dashboard';
+						}else{
+							if(data.level==1){
+								console.log("session business");
+								window.location = '/dashboard';
+							}
+						}
+					}
+				}
+			},
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	return false
+}
+
+$( '#submit' ).click(function() {submitForm()});
+
+document.onkeydown = function () {
+    if (window.event.keyCode == '13') {
+        submitForm();
+    }
+}
