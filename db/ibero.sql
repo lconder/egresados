69138d32a71cd59abfc56691618b5ef49808d55b
Luis Conde <lconder@MacBook-Pro-de-Luis.local>
2017-07-04 14:40:35 -0500

    sql

--- db/ibero.sql
+++ db/ibero.sql
@@ -7,16 +7,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema ibero
--- -----------------------------------------------------
+DROP DATABASE IF EXISTS `ibero`;
 
--- -----------------------------------------------------
--- Schema ibero
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `ibero` DEFAULT CHARACTER SET latin1 ;
 USE `ibero` ;
 
@@ -284,6 +276,81 @@ DEFAULT CHARACTER SET = latin1;
 
 ALTER TABLE `ibero`.`student` 
 CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `user` (`id`,`user`,`password`,`nickname`,`admin`) VALUES (1,'admin','d033e22ae348aeb5660fc2140aec35850c4da997','admin',1);
+
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (1,'Aguascalientes','Ags');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (2,'Baja California','BC');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (3,'Baja California Sur','BCS');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (4,'Campeche','Camp');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (5,'Chiapas','Chis');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (6,'Chihuahua','Chih');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (7,'Coahuila','Cah');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (8,'Colima','Col');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (9,'Ciudad de México','CDMX');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (10,'Durango','Dgo');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (11,'Guanajuato','Gto');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (12,'Guerrero','Gro');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (13,'Hidalgo','Hgo');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (14,'Jalisco','Jal');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (15,'México','Mex');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (16,'Michoacan','Mich');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (17,'Morelos','Mor');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (18,'Nayarit','Nay');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (19,'Nuevo Leon','NL');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (20,'Oaxaca','Oax');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (21,'Puebla','Pue');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (22,'Queretaro','Qro');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (23,'Quintana Roo','QR');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (24,'San Luis Potosi','SLP');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (25,'Sinaloa','Sin');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (26,'Sonora','Son');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (27,'Tabasco','Tab');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (28,'Tamaulipas','Tamps');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (29,'Tlaxcala','Tlax');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (30,'Veracruz','Ver');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (31,'Yucatan','Yuc');
+INSERT INTO `state` (`id_state`,`name`,`short_name`) VALUES (32,'Zacatecas','Zac');
+
+
+INSERT INTO `business_type` (`id_business_type`,`name`) VALUES (1,'SERVICIOS');
+INSERT INTO `business_type` (`id_business_type`,`name`) VALUES (2,'INDUSTRIA');
+INSERT INTO `business_type` (`id_business_type`,`name`) VALUES (3,'COMERCIO');
+INSERT INTO `business_type` (`id_business_type`,`name`) VALUES (4,'AGROPECUARIAS');
+INSERT INTO `business_type` (`id_business_type`,`name`) VALUES (5,'MINERAS');
+
+
+
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (1,'HOTELES',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (2,'ALIMENTOS Y RESTAURANTES',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (3,'SALUD Y BELLEZA',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (4,'GIMNASIOS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (5,'SERVICIO AUTOMOTRIZ',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (6,'ASEGURADORAS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (7,'BIENES RAICES',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (8,'BENEFICIOS DE LA IBERO PUEBLA',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (9,'ESCUELAS Y GUARDERIAS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (10,'FUNERARIAS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (11,'VIAJES',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (12,'EVENTOS Y ESPARCIMIENTO',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (13,'TELECOMUNICACIONES Y VIGILANCIA',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (14,'SERVICIOS PROFESIONALES',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (15,'MASCOTAS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (16,'OTROS SERVICIOS',1);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (17,'FABRICACION DE MUEBLES',2);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (18,'FABRICACION DE PRENDAS DE VESTIR, CALZADO Y ACCESORIOS',2);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (19,'OTRAS DEL SECTOR INDUSTRIA',2);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (20,'ARTICULOS PARA EL HOGAR Y LA OFICINA',3);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (21,'LIBRERIAS',3);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (22,'ROPA, CALZADO Y ACCESORIOS',3);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (23,'OTRAS EMPRESAS DEL SECTOR COMERCIO',3);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (24,'CULTIVO DE FLORES Y FRUTOS',4);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (25,'CULTIVO DE GRANOS Y SEMILLAS',4);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (26,'OTRAS EMPRESAS DEL SECTOR AGROPECUARIO ',4);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (27,'EXTRACCION DE MATERIALES DE CONSTRUCCION',5);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (28,'EXTRACCION DE MATERIA PRIMA DE JOYERIA',5);
+INSERT INTO `categories` (`id_categories`,`name`,`id_business_type`) VALUES (29,'OTRAS DEL SECTOR MINERO',5);
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
