4ed0a296d7a14d02a72d32f85b4af3556d1f1c94
Luis Conde <conderodriguez.luis@outlook.com>
2017-10-11 14:07:15 -0500

    .

--- routes/visit.js
+++ routes/visit.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 function getVisits(id_business, cb){
 	var connection = mysql.createConnection(info_connection);
-	var sql = "SELECT  bp.count, sp.date, b.name, b.address, b.id, p.name as name_promo FROM student_promotions sp INNER JOIN branch_promotions bp ON sp.encrypt_promotion = bp.encrypt INNER JOIN promotions p ON bp.id_promotion = p.id INNER JOIN branch b ON b.id = bp.id_branch WHERE b.business_id=?"
+	var sql = "SELECT  COUNT(bp.encrypt) as count, sp.date, b.name, b.address, b.id, p.name as name_promo FROM student_promotions sp INNER JOIN branch_promotions bp ON sp.encrypt_promotion = bp.encrypt INNER JOIN promotions p ON bp.id_promotion = p.id INNER JOIN branch b ON b.id = bp.id_branch WHERE b.business_id=? GROUP BY sp.date, bp.encrypt"
 	console.log(id_business)
 	connection.query(sql,[id_business],function(err, rows, fields){
 		if(err){
