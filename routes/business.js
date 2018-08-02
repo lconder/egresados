d3c51200b0cda0f6d931fa8c47929f2b37df933b
lconder <brotherowsky@gmail.com>
2018-08-02 11:21:41 -0500

    email

--- routes/business.js
+++ routes/business.js
@@ -346,7 +346,7 @@ function sendEmailtoAdmin(){
 	return new Promise(function(resolve, reject){
 		transporter.sendMail({
 			from: config.user_mail,
-			to: 'michael.sanchez@iberopuebla.mx',
+			to: config.email_admin,
 			subject: 'Nuevo establecimiento registrado',
 			text: 'Se ha ingresado un nuevo convenio ve a tu panel de control para poder activarlo',
 			attachments: [{
